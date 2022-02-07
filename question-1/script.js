889ac4131be74631d39cbc8e02e4fe45e7f11c72
AileenAlbina <70034523+AileenAlbina@users.noreply.github.com>
2022-02-07 02:10:54 -0300

    Update script.js

--- question-1/script.js
+++ question-1/script.js
@@ -4,5 +4,32 @@
  * $ node scritp.js 100
  */
 
-const args = process.argv.slice(-1);
-console.log(`Running question #1 with args ${args}`)
+ const args = process.argv.slice(-1);
+ console.log(`Running question #1 with args ${args}`);
+
+ var forms = calcularFormasDiferentes(args);
+
+ console.log(`para n = ${args} la escalera se puede subir de ${forms} formas diferentes`);
+
+ function calcularFormasDiferentes(arg) {
+
+    let result;
+    let nA = 0;
+    let nB = 1;
+
+    if(arg == 0 )
+        return 0;
+
+    if(arg == 1)
+        return 1;
+
+
+    for (let i = 2; i <= arg; i++) {
+        result = nA+nB
+        nA = nB; 
+        nB = result; 
+    }
+
+    return result;
+
+ }
